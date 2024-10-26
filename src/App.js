28cff2b63cb7c510a7c105f4393f2925fa487186
r s <r@ruddra.ruddra>
2024-10-26 17:49:34 +0530

    just pushing first time

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Example from "./Components/Example";
+import Navbar from "./Components/Navbar"
+function App() {
+  return (
+   <>
+   <Navbar />
+   <Example />
+   </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}",
+      "./node_modules/@shadcn/**/*.js"
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
